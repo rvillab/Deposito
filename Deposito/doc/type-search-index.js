d9e7b7240d81ecd8ff209c2506381ab81783dc9b
rafaelvilla <rvb308@educa.madrid.org>
2024-03-01 00:18:11 +0100

    Creación de JAVADOC para todo el proyecto

--- Deposito/doc/type-search-index.js
+++ Deposito/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cuentas","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cuentas","l":"CCuenta"},{"p":"cuentas","l":"Main"}];updateSearchResults();
